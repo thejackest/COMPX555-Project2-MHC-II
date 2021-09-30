d50b9c0f55899c385dce63337ce3bf48398fc5b9
Jackest Gong <jackest.gong@bluelab.com>
2021-09-30 13:09:49 +1300

    create peptide list from the data file

--- PeptideAffinity.py
+++ PeptideAffinity.py
@@ -3,9 +3,12 @@ class Peptide:
     self.aminoAcid = aminoAcid
     self.isBinding = isBinding
 
-
+peptideList = []
 with open('data.txt') as f:
     for line in f:
-        print(line)
+        words = line.strip("\n").split(" ")
+        peptideList.append(Peptide(words[0], words[1]))
+print(len(peptideList))
+
 
 
