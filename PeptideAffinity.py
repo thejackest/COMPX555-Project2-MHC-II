7a5ed2bc3a53ae5f1b8d39069c66af40a5ae5efb
Jackest Gong <jackest.gong@bluelab.com>
2021-10-19 11:57:35 +1300

    check if position 5 is Q

--- PeptideAffinity.py
+++ PeptideAffinity.py
@@ -1,14 +1,20 @@
-class Peptide:
-  def __init__(self, aminoAcid, isBinding):
-    self.aminoAcid = aminoAcid
-    self.isBinding = isBinding
-
 peptideList = []
 with open('data.txt') as f:
     for line in f:
         words = line.strip("\n").split(" ")
-        peptideList.append(Peptide(words[0], words[1]))
-print(len(peptideList))
+        peptideList.append([words[0], words[1]])
+bind = 0
+notBind = 0
+for peptide in peptideList:
+    if(peptide[0] == '1' and peptide[1][4] == 'Q'):
+        bind += 1
+        print(peptide)
+    elif(peptide[0] == '0' and peptide[1][4] == 'Q'):
+        notBind += 1
+        print(peptide)
+print("bind: " + str(bind))
+print("not bind: " + str(notBind))
+
 
 
 
