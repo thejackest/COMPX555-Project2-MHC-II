1adf26a7f4006b6207a37c271fd027a6fb4b13bb
Jackest Gong <jackest.gong@bluelab.com>
2021-10-26 19:04:21 +1300

    add  cd "/Users/jackestgong/COMPX555/project 02/COMPX555-Project2-MHC-II" ; /usr/bin/env /usr/bin/python3 /Users/jackestgong/.vscode/extensions/ms-python.python-2021.10.1365161279/pythonFiles/lib/python/debugpy/launcher 63260 -- "/Users/jackestgong/COMPX555/project 02/COMPX555-Project2-MHC-II/PeptideAffinity.py"

--- PeptideAffinity.py
+++ PeptideAffinity.py
@@ -24,7 +24,7 @@ def findTheLongestPeptide():
             longest = len(peptide[1])
     print('the longest length is: ' + str(longest))
 
-findTheLongestPeptide()
+# findTheLongestPeptide()
 
 aa = ['W','F','A', 'R', 'N', 'D', 'C', 'Q', 'E','G', 'I', 'L', 'M', 'H', 'P', 'S', 'T', 'Y', 'V', 'K']
 def checkFirstChar(first):
@@ -39,6 +39,20 @@ def checkFirstChar(first):
     print(first + " not bind: " + str(notBind))
 # for peptide in aa:
 #     checkFirstChar(peptide)
+
+def checkLastChar(first):
+    bind = 0
+    notBind = 0
+    for peptide in peptideList:
+        lastIndex = len(peptide[1]) - 1
+        if(peptide[0] == '1' and peptide[1][lastIndex] == first):
+            bind += 1
+        elif(peptide[0] == '0' and peptide[1][lastIndex] == first):
+            notBind += 1
+    print(first + " bind: " + str(bind))
+    print(first + " not bind: " + str(notBind))
+# for peptide in aa:
+#     checkLastChar(peptide)
 
 def printFormattedNumber(num, pos):
     if(pos < 29):
@@ -58,11 +72,70 @@ def checkFrequency(amino, ifBind, positionNumber):
             if(peptide[0] == ifBind and peptide[1][positionNumber] == amino):
                 bind += 1
     printFormattedNumber(bind, positionNumber)
-print(' ', end=": ")
-for i in range(30):
-    printFormattedNumber(i+1, i) 
-for peptide in aa:
-    print(peptide, end=": ")
-    for x in range(30):
-        checkFrequency(peptide, '1', x)
-  
+
+def createFrequencyChart():
+    print(' ', end=": ")
+    for i in range(30):
+        printFormattedNumber(i+1, i) 
+    for peptide in aa:
+        print(peptide, end=": ")
+        for x in range(30):
+            checkFrequency(peptide, '1', x)
+
+def checkPattern():
+    bind = 0
+    notBind = 0
+    for peptide in peptideList:
+        if(peptide[1][0] == 'Q'or'A'or'P' 
+        and peptide[1][1] == 'Q' or 'A' or 'P' or 'L' or 'C' or 'V' or 'F' or 'K' or 'H' 
+        and peptide[1][5] == 'L' or 'V' or 'E'
+        and peptide[1][8] == 'I' or 'L'):
+            if(peptide[0] == '0'):
+                notBind += 1
+            else:
+                bind += 1
+    print("binding: " + str(bind))
+    print( "not binding: " + str(notBind))
+# checkPattern()
+
+bindingList = []
+notBindingList = []
+for peptide in peptideList:
+        if(peptide[0] == '1'):
+            bindingList.append(peptide[1])
+        elif(peptide[0] == '0'):
+            notBindingList.append(peptide[1])
+def calculateAAInPeptide(list):
+    highest = ['',0]
+    for value in aa:
+        rate = 0
+        for peptide in list:
+            if(peptide.find(value) > 0):
+                rate += 1
+        if(rate > highest[1]):
+            highest = [value, rate]
+    print('highest: ',highest)
+# calculateAAInPeptide(bindingList)
+aaaa = []
+for i in aa:
+    for j in aa:
+        aaaa.append(i+j)
+
+def findCharInPeptide():
+    highest = ['',0]
+    for value in aaaa:
+        count = 0
+        rate = 0
+        for peptide in bindingList:
+            if(peptide.find(value) > 0):
+                count += 1
+        rate = count/566
+        count = 0
+        for peptide in notBindingList:
+            if(peptide.find(value) > 0):
+                count += 1
+        rate = rate - (count/962)
+        if(count > highest[1]):
+            highest = [value, rate]
+    print('highest: ',highest)
+findCharInPeptide()
